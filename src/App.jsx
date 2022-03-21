4c28d1f45fb07a2b20da045c0749f93c99b54060
niskuldeep <kuldeepnishad.singh@gmail.com>
2022-03-21 21:45:37 +0530

    sorting not working

---
+++ src/App.jsx
@@ -0,0 +1,186 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  let [data, setData] = React.useState({
+    name: "",
+    gender: "",
+    department: "",
+    role: "",
+    salary: 0,
+  });
+
+  let [dataArray, setDataArray] = React.useState([]);
+
+  let [filterArray, setFilterArray] = React.useState([]);
+
+  let [filterOn, setFilterOn] = React.useState(false);
+
+  let handleChange = (e) => {
+    let { value, checked, name, type } = e.target;
+
+    setData((oldData) => {
+      return {
+        ...oldData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  };
+
+  React.useEffect(() => {
+    getData();
+  }, []);
+
+  function getData() {
+    fetch(`http://localhost:3004/employees`)
+      .then((res) => res.json())
+      .then((res) => setDataArray(res))
+
+      .catch((err) => console.log(err));
+  }
+
+  var showData = "";
+
+  var displayData = (arr) => {
+    showData = arr.map((old) => {
+      return (
+        <div className="box" key={old.id}>
+          <div>Name: {old.name}</div>
+          <div>Gender: {old.gender}</div>
+          <div>Department: {old.department}</div>
+          <div>Role: {old.role}</div>
+          <div>Salary: {old.salary}</div>
+          <br />
+        </div>
+      );
+    });
+  };
+  displayData(dataArray);
+
+  // console.log(data);
+  function addToServer() {
+    const payload = JSON.stringify(data);
+    fetch(`http://localhost:3004/employees`, {
+      method: "POST",
+      body: payload,
+      headers: {
+        "content-type": "application/json",
+      },
+    }).then(() => {
+      getData();
+    });
+  }
+
+  // var showFilterData = "";
+  var showFilterData = filterArray.map((old) => {
+    return (
+      <div className="box" key={old.id}>
+        <div>Name: {old.name}</div>
+        <div>Gender: {old.gender}</div>
+        <div>Department: {old.department}</div>
+        <div>Role: {old.role}</div>
+        <div>Salary: {old.salary}</div>
+        <br />
+      </div>
+    );
+  });
+
+  function filterResult(val) {
+    val == 1 ? setFilterOn(false) : setFilterOn(true);
+    // console.log(filterOn)
+    var temp = dataArray.filter((old) => {
+      return val == old.department;
+    });
+    console.log(temp);
+    setFilterArray(temp);
+  }
+
+  function sortedResult(str) {
+  let sorted = [];
+    if(str == "Asc" ){
+      console.log("ASC")
+      if(filterOn){
+       sorted =  dataArray.sort((a, b) => {
+          return a.salary - b.salary;
+        })
+
+        setDataArray(sorted)
+      }else{
+        sorted =  filterArray.sort((a, b) => {
+          return a.salary-b.salary;
+        })
+        setFilterArray(sorted);
+
+      }
+    }
+    console.log(sorted)
+
+
+  }
+
+  return (
+    <div className="App">
+      <input
+        name="name"
+        type="text"
+        value={data.name}
+        placeholder="Name"
+        onChange={handleChange}
+      />
+      <br />
+
+      <input
+        name="gender"
+        type="text"
+        value={data.gender}
+        placeholder="Gender"
+        onChange={handleChange}
+      />
+      <br />
+
+      <input
+        name="department"
+        type="text"
+        value={data.department}
+        placeholder="Department"
+        onChange={handleChange}
+      />
+      <br />
+
+      <input
+        name="role"
+        type="text"
+        value={data.role}
+        placeholder="Role"
+        onChange={handleChange}
+      />
+      <br />
+
+      <input
+        name="salary"
+        type="number"
+        value={data.salary}
+        placeholder="Salary"
+        onChange={handleChange}
+      />
+      <br />
+
+      <button onClick={addToServer}>ADD EMPLOYEE</button>
+      <hr />
+      <button onClick={() => filterResult(1)}>Show All Department</button>
+      <button onClick={() => filterResult("Marketing")}>Show Marketing</button>
+      <button onClick={() => filterResult("HR")}>Show HR</button>
+      <button onClick={() => filterResult("IT")}>Show IT</button>
+      <button onClick={() => filterResult("Finance")}>Show Finance</button>
+      <hr />
+      <button onClick={() => sortedResult("Asc")}>Sort By Salary Ascending</button>
+      <button onClick={() => sortedResult("Des")}>Sort By Salary Descending</button>
+      <hr />
+      {!filterOn && <div>{showData}</div>}
+      {filterOn && <div>{showFilterData}</div>}
+    </div>
+  );
+}
+
+export default App;
